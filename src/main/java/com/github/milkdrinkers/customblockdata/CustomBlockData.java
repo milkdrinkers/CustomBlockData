acf998677360f31c9072135ecc66b838fcfcff4a
darksaid98 <darksaid9889@gmail.com>
2024-02-21 04:13:31 +0100

    fix(CustomBlockData): add missing copyTo method

--- src/main/java/com/github/milkdrinkers/customblockdata/CustomBlockData.java
+++ src/main/java/com/github/milkdrinkers/customblockdata/CustomBlockData.java
@@ -499,6 +499,11 @@ public class CustomBlockData implements PersistentDataContainer {
         return pdc.isEmpty();
     }
 
+    @Override
+    public void copyTo(@NotNull PersistentDataContainer persistentDataContainer, boolean b) {
+
+    }
+
     @NotNull
     @Override
     public PersistentDataAdapterContext getAdapterContext() {
